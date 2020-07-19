d5ded0d45ad67fcaab9a08defd40a51e15d0b30a
Lars Johannsmeier <lars.johannsmeier@tum.de>
2020-07-19 12:40:14 -0700

    updates

--- robot_install.sh
+++ robot_install.sh
@@ -11,6 +11,11 @@ echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongod
 sudo apt-get update
 sudo apt-get install mongodb-org=4.0.18 mongodb-org-mongos=4.0.18 mongodb-org-server=4.0.18 mongodb-org-tools=4.0.18 mongodb-org-shell=4.0.18
 sudo apt-mark hold mongodb-org*
+sudo sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 sudo systemctl enable mongod.service
 sudo systemctl start mongod.service
+
+
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt install gcc-9 g++-9
 
