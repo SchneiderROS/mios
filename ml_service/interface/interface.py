79d15eb68d62e61d9d8d69c42e23be718b472d8c
ga25mas <samuel.schneider@tum.de>
2020-08-03 22:48:23 +0200

    update

---
+++ ml_service/interface/interface.py
@@ -0,0 +1,20 @@
+import logging
+
+class interface:
+    def __init__(self):
+        pass
+
+    def learn_task(self):
+        pass
+
+    def stop_task(self):
+        pass
+
+    def is_busy(self):
+        pass
+
+    def get_status(self):
+        pass
+
+    def download_results(self):
+        pass
