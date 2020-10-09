134bf4492123c530e3c83dbf5ff292c1adbaa17a
Lars Johannsmeier <lars.johannsmeier@tum.de>
2020-10-09 10:53:34 +0200

    removed automatic start of roscore and adapted docker images
    Former-commit-id: b6f9f80ccadc7c76b1f6bd0f16a7b93542bbe96d

--- docker/build_docker_image_ml_service.sh
+++ docker/build_docker_image_ml_service.sh
@@ -5,3 +5,4 @@ cd ${ROOT}
 
 docker build -t mios_mls -f docker/ml_service/Dockerfile .
 docker tag mios_mls:latest msrm/mios_mls:release
+docker tag mios_mls:latest collective-control-001.local:5000/mios_mls
