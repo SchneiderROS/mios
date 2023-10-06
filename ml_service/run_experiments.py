846980727d39c5375718a08131aeca864080b0f9
Samuel Schneider <samuel.schneider@tum.de>
2023-10-06 16:27:29 +0200

    5agent experiment setup
    Former-commit-id: b05872d55cc36a382dd07388274e3245f3f695d1

--- ml_service/run_experiments.py
+++ ml_service/run_experiments.py
@@ -432,9 +432,7 @@ def test_cutoff(cutoff ={ '001_left': 0.7080000000000001,   # best solution foun
 
 def five_agent_collective():
     modules = list_block_1 + list_block_2 + list_U
-    tasks = {}
-    for xxx in modules: 
-        tasks["collective-"+str(xxx)+".rsi.ei.tum.de"] = [str(xxx)+"_left"]
+    
 
     cutoff = {  '001_left': 0.7080000000000001,   # best solution found
                 '003_left': 0.68016,
@@ -465,6 +463,9 @@ def five_agent_collective():
     sc = SVMLearner(450,10,0,True,False, 0.4,True).get_configuration()
     tags = ["5agents_25tasks", "collective"]
     for n_current_iter in range(10):
+        tasks = {}
+        for xxx in modules: 
+            tasks["collective-"+str(xxx)+".rsi.ei.tum.de"] = [str(xxx)+"_left"]
         threads = []
         print("Number of iteration: ", n_current_iter+1)
         knowledge_source = Knowledge()
