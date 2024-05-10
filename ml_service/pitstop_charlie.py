0ab026b49f5755f7f8838458a9c1793f45c3d623
Samuel Schneider <samuel.schneider@tum.de>
2024-05-10 08:33:50 +0200

    remove false tag; increase readyness check robustness
    Former-commit-id: b60d46b2a504c3064996ffb71df5ee08d8500624

--- ml_service/pitstop_charlie.py
+++ ml_service/pitstop_charlie.py
@@ -2,6 +2,7 @@ from copy import deepcopy
 from http import client
 
 from pathspec import iter_tree
+from problem_definition.domain import Domain
 from run_experiments import *
 
 # ---------------------------- exp robots ------------------------------------
@@ -46,10 +47,12 @@ def prefill_fast_pipe(iteration_n:int, kb_location:str, tags: list = ["10agents_
     data = client.read("global_ml_results","insertion", {"meta.tags":tags})
     for results in data: 
         task_ident = str(results["meta"]["tags"])
+        domain = Domain.from_dict(results["meta"]["doamin"])
         for i in range(1,len(results)-3+1):
             try:
                 if results["n"+str(i)]["q_metric"]["success"]:
-                    kb.push_trial(task_ident, results["n"+str(i)]["theta"],results["n"+str(i)]["q_metric"]["final_result"], 1000)
+                    theta_normalised = domain.normalize(results["n"+str(i)]["theta"])
+                    kb.push_trial(task_ident, theta_normalised, results["n"+str(i)]["q_metric"]["final_result"], 1000)
                 else:
                     continue
             except KeyError:
@@ -61,7 +64,8 @@ def collective25(n_current_iter:int, tags_addon:list = ["100collective","ps_char
     n_current_iter: number of current iteration
 
     '''
-    tags = ["ps_alpha_5","10agents_25tasks", "round1"]
+
+    tags = ["ps_alpha_5","10agents_25tasks"]
     # tags = ["100_testing"]
     # modules = list_block_1 + list_block_2 + list_U
     
@@ -92,7 +96,7 @@ def collective25(n_current_iter:int, tags_addon:list = ["100collective","ps_char
                 '040_left': 0.61824,
                 '029_left': 0.68088}
     # sc = SVMLearner(450,10,0,True,False, 0.4,True).get_configuration()
-    sc = SVMLearner(450,10,0,True,False, 0.4,True).get_configuration()
+    sc = SVMLearner(150,10,0,True,False, 0.4,True).get_configuration()
         
     # for n_current_iter in range(29,30): #range(15,25):   (not reserve)
 
@@ -125,25 +129,20 @@ def collective25(n_current_iter:int, tags_addon:list = ["100collective","ps_char
                 "collective-029.rsi.ei.tum.de":["A_018_cross-2", "A_016_cross-1"]
             }
 
-
     threads = []
     print("Number of iteration: ", n_current_iter+1)
     knowledge_source = Knowledge()
     knowledge_source.kb_location = "collective-001.rsi.ei.tum.de" # None #  
     knowledge_source.mode = "global"  # None:isolated parallel (no knowledge from theirself)  # "local": has transfer inside agent
-    knowledge_source.scope = [] # TODO: may here add the tag of previous running
+    knowledge_source.scope = []
     knowledge_source.scope.extend(deepcopy(tags))
     knowledge_source.scope.append("n"+str(n_current_iter+1)) # searching for knowledge on the database (only works for the slow pipeline);  e.g. [] search all, 
     knowledge_source.type = "all"  # all: 
         
-    
-
     threads = []
     tags.extend(tags_addon)
     while len(tasks) > 0:
         for robot in tasks:
-            
-            
             dualarm_skills = []
             move_context = {
                         "skill": {
@@ -159,30 +158,26 @@ def collective25(n_current_iter:int, tags_addon:list = ["100collective","ps_char
                             "F_ext_max": [100, 50]}}
             dualarm_skills.append(("move", "MoveToPoseJoint", move_context))
             
-            kb = ServerProxy("http://" + knowledge_source.kb_location+ ":8001", allow_none=True)
-            kb.clear_memory()
-            
             server = ServerProxy("http://%s:%s/" %(robot, "8000"))
             if len(tasks[robot]) == 0:
                 tasks.pop(robot)
                 continue
             if server.is_busy():
                 continue
-            insertable = tasks[robot][0]
             if not check_object(robot, insertable):
                 continue
             if sum([t.is_alive() for t in threads]) >= n_agents:
-                time.sleep(1)
+                continue
+            if not get_states([robot.split(".")[0][-3:]])[0]:
+                print(robot, "is not ready! Skipping task ",insertable)
                 continue
 
+            insertable = tasks[robot].pop(0)
             container = insertable+"_container" 
             approach = container+"_approach"
             pd = InsertionFactory([robot], TimeMetric("insertion", {"time": 5}),
                                     {"Insertable": insertable, "Container": container,
                                     "Approach": approach}).get_problem_definition(insertable)
-            if not get_states([robot.split(".")[0][-3:]])[0]:
-                print(robot, "is not ready! Skipping task ",insertable)
-                continue
             if insertable in cutoff:
                 sc.finish_cost = cutoff[insertable]
             else:
@@ -214,13 +209,3 @@ def check_object(host, obj):
                 move_joint(host, "raise_hand")
             print("wainting for ",host, " to grasp ", obj)
             return False
-
-def alpha_experiment():
-    for i in range(10):  # iteration
-        for n in range(6,10):
-            var_agent_collective(n,i, [str(n)+"agents_25tasks","collective","ps_alpha_var"])
-
-def alpha_experiment_second():
-    for i in range(10):  # iteration
-        for n in range(1,5):
-            var_agent_collective(n,i, [str(n)+"agents_25tasks","collective","ps_alpha_var"])
